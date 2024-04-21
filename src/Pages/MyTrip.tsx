9af9a4b71f1360bb40511e266304b18a55cf3cec
Tex <techcenter2001@gmail.com>
2024-04-22 00:42:13 +0300

    update mytrips page

--- src/Pages/MyTrip.tsx
+++ src/Pages/MyTrip.tsx
@@ -1,27 +1,37 @@
-import * as React from 'react';
-import { useState } from 'react';
-import NavBar from './SharedComponents/NavBar';
-import Footer from './SharedComponents/Footer';
+import * as React from "react";
+import { useState } from "react";
+import NavBar from "./SharedComponents/NavBar";
+import Footer from "./SharedComponents/Footer";
 
-import { Box, Typography, Card, CardContent, IconButton, Button, CardMedia, Select, MenuItem ,SelectChangeEvent } from '@mui/material';
+import {
+  Box,
+  Typography,
+  Card,
+  CardContent,
+  IconButton,
+  Button,
+  CardMedia,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
 
-import AddIcon from '@mui/icons-material/Add';
-import SavedSearchIcon from '@mui/icons-material/SavedSearch';
-import PlaceIcon from '@mui/icons-material/Place';
-import DateRangeIcon from '@mui/icons-material/DateRange';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import AddIcon from "@mui/icons-material/Add";
+import SavedSearchIcon from "@mui/icons-material/SavedSearch";
+import PlaceIcon from "@mui/icons-material/Place";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
-import Tripimage from '../images/MyTrip/Tripimage.png';
-import Background from '../images/MyTrip/Background.png';
+import Tripimage from "../images/MyTrip/Tripimage.png";
+import Background from "../images/MyTrip/Background.png";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const MyTrip = () => {
-
-  const [sort, setSort] = useState('');
+  const [sort, setSort] = useState("");
 
   const handleSortChange = (event: SelectChangeEvent) => {
     setSort(event.target.value);
@@ -31,62 +41,151 @@ const MyTrip = () => {
   return (
     <div>
       <Box
+        style={{
+          backgroundColor: "teal",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          paddingBottom: "10px",
+          width: "100%",
+        }}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box sx={{ width: "1280px" }}>
+          <NavBar textColor="rgb(255,255,255)" />
+        </Box>
+      </Box>
+
+      <Box
         display="flex"
         flexDirection="column"
         alignItems="center"
-        sx={{ width: '100%', maxWidth: 1280, margin: 'auto',backgroundImage: `url(${Background})` }}
-      >
+        sx={{
+          width: "100%",
 
+          margin: "auto",
+          backgroundImage: `url(${Background})`,
+          backgroundRepeat: "no-repeat",
+        }}
+      >
         <Box
-          style={{ backgroundColor: 'teal', backgroundSize: 'cover', backgroundPosition: 'center', paddingBottom: '10px', width: '100%' }}
-          sx={{ mb: 15, maxWidth: 1280, margin: 'auto' }}
+          sx={{
+            width: "90%",
+            maxWidth: 1280,
+            marginTop: "120px",
+            padding: "20px",
+            display: "flex",
+            flexDirection: "column",
+          }}
         >
-          <NavBar textColor="rgb(255,255,255)"/>
-        </Box>
-        
-        <Box sx={{ width: '90%', maxWidth: 1280, marginTop: '120px', padding: '20px', display: 'flex', flexDirection: 'column'  }}>
-          <Typography variant="h2" sx={{ fontWeight: 'bold' }}>My Trip</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', marginTop: '20px' }}>
-            <Button variant="contained" style={{ backgroundColor: 'sandybrown', padding: '40px', borderRadius: '20px' }} sx={{ flex: 1 }}><AddIcon /> Create you own trip</Button>
-            <Button variant="contained" style={{ backgroundColor: 'sandybrown', padding: '40px', borderRadius: '20px' }} sx={{ marginLeft: '30px', flex: 1 }}><SavedSearchIcon/>Generate your own trip</Button>
+          <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+            My Trip
+          </Typography>
+          <Box
+            sx={{ display: "flex", flexDirection: "row", marginTop: "20px" }}
+          >
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "sandybrown",
+                padding: "40px",
+                borderRadius: "20px",
+              }}
+              sx={{ flex: 1 }}
+            >
+              <AddIcon /> Create you own trip
+            </Button>
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "sandybrown",
+                padding: "40px",
+                borderRadius: "20px",
+              }}
+              sx={{ marginLeft: "30px", flex: 1 }}
+            >
+              <SavedSearchIcon />
+              Generate your own trip
+            </Button>
           </Box>
 
-          <Box sx={{ width: '90%', maxWidth: 1280, marginTop: '120px', padding: '20px', display: 'flex', flexDirection: 'column'  }} >
-            <Box sx={{display:'flex', justifyContent: 'flex-end',alignItems:'center'}}>
+          <Box
+            sx={{
+              width: "100%",
+
+              marginTop: "120px",
+              padding: "20px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+              }}
+            >
               <Typography variant="h6">Sort By:</Typography>
-              <Select value={sort} onChange={handleSortChange} sx={{width:'20%', marginLeft:'10px'}} variant="standard">
-                <MenuItem value={'name'}>Name</MenuItem>
-                <MenuItem value={'date'}>Created date</MenuItem>
+              <Select
+                value={sort}
+                onChange={handleSortChange}
+                sx={{ width: "20%", marginLeft: "10px" }}
+                variant="standard"
+              >
+                <MenuItem value={"name"}>Name</MenuItem>
+                <MenuItem value={"date"}>Created date</MenuItem>
                 {/* Add more sorting options here */}
               </Select>
             </Box>
-            
-            <Card sx={{marginTop:'50px',borderRadius: '20px'}}>
-              <Box display="flex" sx={{backgroundColor: 'powderblue', position: 'relative'}}>
-                <IconButton style={{position: 'absolute', top: '10px', right: '10px'}}>
+
+            <Card sx={{ marginTop: "50px", borderRadius: "20px" }}>
+              <Box
+                display="flex"
+                sx={{ backgroundColor: "powderblue", position: "relative" }}
+              >
+                <IconButton
+                  style={{ position: "absolute", top: "10px", right: "10px" }}
+                >
                   <MoreHorizIcon />
                 </IconButton>
                 <CardMedia
                   component="img"
                   image={Tripimage}
                   alt="Trip image"
-                  style={{width: '40%'}}
+                  style={{ width: "40%" }}
                 />
                 <Box p={2}>
                   <CardContent>
                     <Typography variant="h5" component="div">
                       Trip Name
                     </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{marginBottom:'10px',marginTop:'10px'}}>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      sx={{ marginBottom: "10px", marginTop: "10px" }}
+                    >
                       Small description about the Trip places and Days
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      <PlaceIcon/> Muharraq, Northern
+                      <PlaceIcon /> Muharraq, Northern
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      <DateRangeIcon/> 5 Days
+                      <DateRangeIcon /> 5 Days
                     </Typography>
-                    <Button variant="contained" color="primary" style={{backgroundColor: 'darkslategray',position: 'absolute', bottom: '10px', right: '10px',borderRadius: '20px'}}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      style={{
+                        backgroundColor: "darkslategray",
+                        position: "absolute",
+                        bottom: "10px",
+                        right: "10px",
+                        borderRadius: "20px",
+                      }}
+                    >
                       Start Trip
                     </Button>
                   </CardContent>
@@ -94,32 +193,51 @@ const MyTrip = () => {
               </Box>
             </Card>
 
-            <Card sx={{marginTop:'50px',borderRadius: '20px'}}>
-              <Box display="flex" sx={{backgroundColor: 'powderblue', position: 'relative'}}>
-                <IconButton style={{position: 'absolute', top: '10px', right: '10px'}}>
+            <Card sx={{ marginTop: "50px", borderRadius: "20px" }}>
+              <Box
+                display="flex"
+                sx={{ backgroundColor: "powderblue", position: "relative" }}
+              >
+                <IconButton
+                  style={{ position: "absolute", top: "10px", right: "10px" }}
+                >
                   <MoreHorizIcon />
                 </IconButton>
                 <CardMedia
                   component="img"
                   image={Tripimage}
                   alt="Trip image"
-                  style={{width: '40%'}}
+                  style={{ width: "40%" }}
                 />
                 <Box p={2}>
                   <CardContent>
                     <Typography variant="h5" component="div">
                       Trip Name
                     </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{marginBottom:'10px',marginTop:'10px'}}>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      sx={{ marginBottom: "10px", marginTop: "10px" }}
+                    >
                       Small description about the Trip places and Days
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      <PlaceIcon/> Muharraq, Northern
+                      <PlaceIcon /> Muharraq, Northern
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                      <DateRangeIcon/> 5 Days
+                      <DateRangeIcon /> 5 Days
                     </Typography>
-                    <Button variant="contained" color="primary" style={{backgroundColor: 'darkslategray',position: 'absolute', bottom: '10px', right: '10px',borderRadius: '20px'}}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      style={{
+                        backgroundColor: "darkslategray",
+                        position: "absolute",
+                        bottom: "10px",
+                        right: "10px",
+                        borderRadius: "20px",
+                      }}
+                    >
                       Start Trip
                     </Button>
                   </CardContent>
@@ -129,11 +247,16 @@ const MyTrip = () => {
           </Box>
         </Box>
 
-        <Box sx={{ width: '100%', maxWidth: 1280, margin: 'auto', marginTop: '200px' }}>
-          <Footer />
-        </Box>
-
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: 1280,
+            margin: "auto",
+            marginTop: "200px",
+          }}
+        ></Box>
       </Box>
+      <Footer />
     </div>
   );
 };
