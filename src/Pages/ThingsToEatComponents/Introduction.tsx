b043f536759fbd23632eb67b86ef100446fbc5bb
Bisho9029 <128395644+Bisho9029@users.noreply.github.com>
2024-04-29 22:52:16 +0300

    Add files via upload

--- src/Pages/ThingsToEatComponents/Introduction.tsx
+++ src/Pages/ThingsToEatComponents/Introduction.tsx
@@ -5,11 +5,10 @@ import * as React from "react";
 import {
   Box,
   Typography,
-  Button,
   Rating,
-  Grid,
   useMediaQuery,
   useTheme,
+  Button
 } from "@mui/material";
 
 import LocalDiningIcon from "@mui/icons-material/LocalDining";
@@ -45,72 +44,92 @@ const Introduction = () => {
         </Typography>
         <Button style={{ color: "black" }}>Save</Button>
       </Box>
-      <Grid container spacing={2} sx={{ p: { xs: 2 } }}>
-        <Grid item xs={isMobile ? 12 : 6}>
-          <Typography variant="h4" component="div">
-            Bushido by Buddha-Bar
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary">
-            <LocationOnIcon /> Road 38 Building Nr 52, Block 428, Seef Area, to
-            the right of Ritz Carlton Hotel, Manama Bahrain
-          </Typography>
-          <Typography
-            variant="overline"
-            sx={{ marginTop: "20px", marginBottom: "20px" }}
-          >
-            <Box display="flex" alignItems="center">
-              <LocalDiningIcon />
-              Restaurant | 4.8
-              <Rating name="place-rating" value={4} readOnly />
-              <Typography variant="body2" sx={{ fontSize: "0.7em" }}>
-                1,750 reviews
-              </Typography>
+      <Box
+        display="flex"
+        flexDirection={isMobile ? "column" : "row"}
+        alignItems="center"
+        justifyContent="center"
+        gap={2}
+      >
+        <Box flex={2}>
+          <Box>
+            <Typography variant="h4" component="div">
+              Bushido by Buddha-Bar
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary">
+              <LocationOnIcon /> Road 38 Building Nr 52, Block 428, Seef Area,
+              to the right of Ritz Carlton Hotel, Manama Bahrain
+            </Typography>
+            <Typography
+              variant="overline"
+              sx={{ marginTop: "20px", marginBottom: "20px" }}
+            >
+              <Box display="flex" alignItems="center">
+                <LocalDiningIcon />
+                Restaurant | 4.8
+                <Rating name="place-rating" value={4} readOnly />
+                <Typography variant="body2" sx={{ fontSize: "0.7em" }}>
+                  1,750 reviews
+                </Typography>
+              </Box>
+            </Typography>
+            <Typography variant="body1">
+              Bushido by Buddha-Bar blends traditional Japanese instruments with
+              modern electronic beats, capturing the essence of the samurai
+              code. This mesmerizing compilation evokes images of Zen gardens
+              and martial arts, offering a unique auditory experience that
+              resonates with reverence and adventure. With tracks crafted to
+              evoke cherry blossoms, misty mountains, and the elegance of
+              geishas, it's a captivating journey into Japanese culture.
+            </Typography>
+          </Box>
+        </Box>
+        <Box flex={1}>
+          <Box>
+            <Box
+              display="flex"
+              flexDirection={isMobile ? "column" : "row"}
+              alignItems="center"
+              justifyContent="center"
+              gap={2} // Add this line
+            >
+              <Box
+                flex={2}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <img src={Cardimage1} alt="Card Image 1" />
+              </Box>
+              <Box
+                flex={1}
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                gap={2} // Add this line
+              >
+                <Box
+                  flex={1}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <img src={Cardimage2} alt="Card Image 2" />
+                </Box>
+                <Box
+                  flex={1}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <img src={Cardimage3} alt="Card Image 3" />
+                </Box>
+              </Box>
             </Box>
-          </Typography>
-          <Typography variant="body1">
-            Bushido by Buddha-Bar blends traditional Japanese instruments with
-            modern electronic beats, capturing the essence of the samurai code.
-            This mesmerizing compilation evokes images of Zen gardens and
-            martial arts, offering a unique auditory experience that resonates
-            with reverence and adventure. With tracks crafted to evoke cherry
-            blossoms, misty mountains, and the elegance of geishas, it's a
-            captivating journey into Japanese culture.
-          </Typography>
-        </Grid>
-        <Grid item xs={isMobile ? 12 : 6}>
-          <Grid container direction="row">
-            <Grid item xs={6}>
-              <img
-                src={Cardimage1}
-                alt="Card Image 1"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                  borderRadius: "6px 0px 0px 6px",
-                }}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <img
-                src={Cardimage2}
-                alt="Card Image 2"
-                style={{ width: "100%", height: "50%", objectFit: "cover" }}
-              />
-              <img
-                src={Cardimage3}
-                alt="Card Image 3"
-                style={{
-                  width: "100%",
-                  height: "50%",
-                  objectFit: "cover",
-                  marginTop: "-4px",
-                }}
-              />
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+          </Box>
+        </Box>
+      </Box>
     </div>
   );
 };
